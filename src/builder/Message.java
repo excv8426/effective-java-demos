3b9030f9847bfab3f76b27b11d154ca23b3f707f
excv8426 <598867208@qq.com>
2016-03-05 14:50:43 +0800

    builder

---
+++ src/builder/Message.java
@@ -0,0 +1,46 @@
+package builder;
+
+public final class Message {
+	private String sender;
+	private String receiver;
+	private String content;
+	
+	
+	
+	public String getSender() {
+		return sender;
+	}
+
+	public String getReceiver() {
+		return receiver;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public static class Builder{
+		private String sender;
+		private String receiver;
+		private String content="";
+		
+		public Builder(String s,String r){
+			this.sender=s;
+			this.receiver=r;
+		}
+		public Builder content(String c){
+			this.content=c;
+			return this;
+		}
+		public Message build(){
+			return new Message(this);
+		}
+	}
+	
+	private Message(Builder builder){
+		sender=builder.sender;
+		receiver=builder.receiver;
+		content=builder.content;
+	}
+	
+}
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,12 +1,14 @@
 package test;
 
+import builder.Message;
 import staticfactorymethod.Teststaticfactorymethod;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Teststaticfactorymethod.newService1();
-
+		//Teststaticfactorymethod.newService1();
+		Message message=new Message.Builder("ssss", "rrrrr").content("cccc").build();
+		System.out.println(message.getSender()+message.getReceiver()+message.getContent());
 	}
 
 }
