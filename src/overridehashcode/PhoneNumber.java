3f177ffc7e9192ed48cac2de55b99b31dfef5dfe
excv8426 <598867208@qq.com>
2016-03-11 21:54:30 +0800

    2154

--- src/overridehashcode/PhoneNumber.java
+++ src/overridehashcode/PhoneNumber.java
@@ -7,4 +7,35 @@ public class PhoneNumber {
 		this.areaCode=areaCode;
 		this.lineNumber=lineNumber;
 	}
+	public int getAreaCode() {
+		return areaCode;
+	}
+	public void setAreaCode(int areaCode) {
+		this.areaCode = areaCode;
+	}
+	public int getLineNumber() {
+		return lineNumber;
+	}
+	public void setLineNumber(int lineNumber) {
+		this.lineNumber = lineNumber;
+	}
+	
+	@Override
+	public int hashCode(){
+		return 42;
+	}
+	
+	@Override
+	public boolean equals(Object object){
+		if (object==this) {
+			return true;
+		} else {
+			if (object instanceof PhoneNumber) {
+				PhoneNumber phoneNumber=(PhoneNumber) object;
+				return (phoneNumber.areaCode==this.areaCode)&&(phoneNumber.lineNumber==this.lineNumber);
+			} else {
+				return false;
+			}
+		}
+	}
 }
--- src/overridehashcode/Testhashcode.java
+++ src/overridehashcode/Testhashcode.java
@@ -1,11 +1,22 @@
 package overridehashcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Testhashcode {
 
 	public static void test(){
 		PhoneNumber p1=new PhoneNumber(0335, 123456);
-		PhoneNumber p2=new PhoneNumber(0335, 123456);
-		System.out.println(p1.hashCode());
-		System.out.println(p2.hashCode());
+		PhoneNumber p2=new PhoneNumber(0335, 1234567);
+		PhoneNumber p3=new PhoneNumber(0335, 123456);
+		/*System.out.println(p1.hashCode());
+		System.out.println(p2.hashCode());*/
+		
+		Map<PhoneNumber, String> map=new HashMap<>();
+		map.put(p1, "p1");
+		System.out.println(map.get(p1));
+		System.out.println(map.get(p2));
+		System.out.println(map.get(p3));
+		System.out.println(map.get(new PhoneNumber(0335, 123456)));
 	}
 }
